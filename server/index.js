11d66a98918d8d9d1937747071b43fca05725fa9
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-05-16 12:09:45 +0100

    added basic server code

--- server/index.js
+++ server/index.js
@@ -1,1 +1,9 @@
-console.log("server started");
+const express = require("express");
+
+const PORT = 8080;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Note Taker is listening on http://localhost:${PORT}`);
+});
