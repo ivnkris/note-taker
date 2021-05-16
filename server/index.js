4c70d9a4408ef62615b22d4ea225b7f791653f31
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-05-16 12:17:36 +0100

    added basic router and static middlewares with some other standard utility middlewares

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 
+const apiRoutes = require("./routes/api-routes");
+const htmlRoutes = require("./routes/html-routes");
+
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`Note Taker is listening on http://localhost:${PORT}`);
